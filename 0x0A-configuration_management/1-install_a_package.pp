26ae895eb0ab7aeccc3a01a0ce2f8cb26d64edb8
lanrewest <lanrewese@yahoo.com>
2023-04-23 07:56:19 -0700

    packageinstall

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -7,8 +7,13 @@ package { 'python3-pip':
   ensure => 'installed',
 }
 
+exec { 'update pip3':
+  command => '/usr/bin/pip3 install --upgrade pip',
+}
+
 exec { 'install flask':
   command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => '/usr/local/bin:/usr/bin:/bin',
   unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
 
