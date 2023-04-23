62794e1c72c12a387e46733a21cda0423279556f
lanrewest <lanrewese@yahoo.com>
2023-04-23 08:03:35 -0700

    packageinstall

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -11,9 +11,23 @@ exec { 'update pip3':
   command => '/usr/bin/pip3 install --upgrade pip',
 }
 
+package { 'libxml2-dev':
+  ensure => 'installed',
+}
+
+package { 'libxslt1-dev':
+  ensure => 'installed',
+}
+
+exec { 'install markupsafe':
+  command => '/usr/bin/pip3 install markupsafe',
+  unless  => '/usr/bin/pip3 show markupsafe',
+}
+
 exec { 'install flask':
   command => '/usr/bin/pip3 install flask==2.1.0',
   path    => '/usr/local/bin:/usr/bin:/bin',
   unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
+
 
