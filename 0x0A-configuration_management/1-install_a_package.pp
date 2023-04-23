d171f966e282b17d1c95b2f54eca20c36226c967
lanrewest <lanrewese@yahoo.com>
2023-04-23 07:49:48 -0700

    packageinstall

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,14 @@
-# puppet install a package
+#package install a package
 
 package { 'puppet-lint':
   ensure  => present,
 }
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+exec { 'install flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+}
+
